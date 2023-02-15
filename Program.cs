02da2d5ff18d25281447d50c0de43377f8b6c349
gabriel.oliveira32 <gabriel.oliveira32@AME0635270W10-1>
2023-02-15 11:22:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios15Fevereiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.WriteLine("Digite um nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
+        }
+    }
+}
